4d7f6e585cc72001b016d73687a9f367cffc637b
eduardoreyes007351208 <lalosoccer307@gmail.com>
2023-07-25 10:41:23 -0700

    complete signin

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { UserAuth } from '../context/AuthContext'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+
+const style = {
+    bg: 'max-w-[700px] mx-auto my-16 p-4',
+    heading: `text-2xl font-bold py-2 text-[#B85042]`,
+    paragraph: `text-[#B85042]`,
+    form: `flex flex-col py-2`,
+    label: `py-2 font-medium text-[#B85042]`,
+    input: `border p-3 rounded-md`,
+    button: `border boarder-[#A7BEAE] bg-[#A7BEAE] hover:bg-[#B85042] hover:text-white w-full p-4 text-white rounded-md`
+}
 
 const Signin = () => {
     const [email, setEmail] = useState('')
@@ -24,20 +34,23 @@ const Signin = () => {
     }, [])
 
   return (
-    <div>
+    <div className={style.bg}>
         <div>
-            <h3>Sign In</h3>
+            <h3 className={style.heading}>Sign in to your account. </h3>
+            <p className={style.paragraph}>
+                Don't have an account? <Link to='/signup' className='underline'> Sign up </Link>
+            </p>
         </div>
         <form onSubmit={handleSubmit}>
-            <div>
-              <label>Email</label>
-              <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+            <div className={style.form}>
+              <label className={style.label}>Email</label>
+              <input className={style.input} value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
             </div>  
-            <div>
-              <label>Password</label>
-              <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+            <div className={style.form}>
+              <label className={style.label}>Password</label>
+              <input className={style.input} value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
             </div>
-            <button>Sign In</button>
+            <button className={style.button}>Sign In</button>
         </form>
     </div>
   )

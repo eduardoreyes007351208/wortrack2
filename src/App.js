965026e6efd528d83fd71dd0bef2bd3e4a16b8a3
eduardoreyes007351208 <lalosoccer307@gmail.com>
2023-07-25 10:05:25 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import {Route, Routes} from 'react-router-dom'
 import Account from './components/account'
 import Signin from './components/signin'
 import Signup from './components/signup'
+import { AuthContextProvider } from './context/AuthContext'
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
-      <Routes>
-        <Route path='/account' element={<Account/>} />
-        <Route path='/' element={<Signin/>} />
-        <Route path='/signup' element={<Signup/>} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/account' element={<Account/>} />
+          <Route path='/' element={<Signin/>} />
+          <Route path='/signup' element={<Signup/>} />
+        </Routes>
+      </AuthContextProvider>
+      
     </div>
   );
 }
--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { UserAuth } from '../context/AuthContext'
 
 const Signin = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const {signIn} = UserAuth()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await signIn(email, password)
+        } catch (e) {
+            alert('Account not found')
+            console.log(e.message)
+        }
+    }
+
   return (
-    <div>signin</div>
+    <div>
+        <div>
+            <h3>Sign In</h3>
+        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+              <label>Email</label>
+              <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+            </div>  
+            <div>
+              <label>Password</label>
+              <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+            </div>
+            <button>Sign In</button>
+        </form>
+    </div>
   )
 }
 

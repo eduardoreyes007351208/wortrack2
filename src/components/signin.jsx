f547079c42e0ab212c3bd47eb250b3015914e67f
eduardoreyes007351208 <lalosoccer307@gmail.com>
2023-07-25 10:30:05 -0700

    commit

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 const Signin = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {signIn, user} = UserAuth()
+    const {signIn} = UserAuth()
     const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
